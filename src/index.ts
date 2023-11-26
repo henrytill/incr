c112c5421a3277f282cdbde2d7961afa2bdb774f
Henry Till <henrytill@gmail.com>
2023-11-25 22:35:30 -0800

    add initial work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,92 @@
-export const add = (a: number, b: number) => a + b;
+import * as crypto from 'node:crypto';
+
+export type Dependency<A, B> = Target<A, B> | Leaf<A, B>;
+
+export type Builder<A, B> = (...deps: Dependency<A, B>[]) => B;
+
+export class Leaf<A, B> {
+  readonly id: string;
+
+  value: A;
+
+  parents: Target<A, B>[];
+
+  constructor(value: A) {
+    this.id = crypto.randomUUID();
+    this.value = value;
+    this.parents = [];
+  }
+
+  dependencyOf(target: Target<A, B>): void {
+    this.parents.push(target);
+  }
+
+  update(): void {
+    for (const parent of this.parents) {
+      parent.update();
+    }
+  }
+
+  accept(visitor: ItemVisitor<A, B>): void {
+    visitor.visitLeaf(this);
+  }
+}
+
+export class Target<A, B> {
+  readonly id: string;
+
+  value: B;
+
+  parents: Target<A, B>[];
+
+  children: Dependency<A, B>[];
+
+  builder: Builder<A, B>;
+
+  constructor(value: B, deps: Dependency<A, B>[], builder: Builder<A, B>) {
+    this.id = crypto.randomUUID();
+    this.value = value;
+    this.parents = [];
+    this.children = deps;
+    this.builder = builder;
+
+    for (const c of this.children) {
+      c.dependencyOf(this);
+    }
+  }
+
+  dependencyOf(target: Target<A, B>): void {
+    this.parents.push(target);
+  }
+
+  dependsOn(...deps: Dependency<A, B>[]): void {
+    this.children.push(...deps);
+  }
+
+  build(): void {
+    for (const child of this.children) {
+      if (child instanceof Leaf) {
+        continue;
+      }
+      child.build();
+    }
+    this.value = this.builder(...this.children);
+  }
+
+  update(): void {
+    this.value = this.builder(...this.children);
+    for (const parent of this.parents) {
+      parent.update();
+    }
+  }
+
+  accept(visitor: ItemVisitor<A, B>): void {
+    visitor.visitTarget(this);
+  }
+}
+
+export abstract class ItemVisitor<A, B> {
+  abstract visitLeaf(leaf: Leaf<A, B>): void;
+
+  abstract visitTarget(target: Target<A, B>): void;
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,52 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-import { add } from '../src/index.js';
+import { Dependency, ItemVisitor, Leaf, Target } from '../src/index.js';
 
-test('add', (t) => {
-  assert.strictEqual(add(1, 2), 3);
+test('basic', () => {
+  const y: Leaf<number, number> = new Leaf(2);
+  const z: Leaf<number, number> = new Leaf(3);
+  const x: Target<number, number> = new Target(0, [y, z], (a, b) => a.value + b.value);
+
+  assert.deepStrictEqual(x.children, [y, z]);
+  assert.deepStrictEqual(y.parents, [x]);
+  assert.deepStrictEqual(z.parents, [x]);
+
+  assert.strictEqual(x.value, 0);
+  x.update();
+  assert.strictEqual(x.value, 5);
+
+  y.value = 4;
+  y.update();
+  assert.strictEqual(x.value, 7);
+
+  z.value = 5;
+  z.update();
+  assert.strictEqual(x.value, 9);
+});
+
+test('compound', () => {
+  const y: Leaf<number, number> = new Leaf(2);
+  const z: Leaf<number, number> = new Leaf(3);
+  const x: Target<number, number> = new Target(0, [y, z], (a, b) => a.value + b.value);
+  const w: Target<number, number | string> = new Target(
+    '',
+    [x as Dependency<number, number | string>],
+    (a) => `foo${a.value}`,
+  );
+
+  assert.deepStrictEqual(x.parents, [w]);
+  assert.deepStrictEqual(w.children, [x]);
+  x.update();
+  assert.strictEqual(w.value, 'foo5');
+
+  const visitor: ItemVisitor<number, number | string> = {
+    visitLeaf: (l: Leaf<number, number>) => {
+      l.value = 1;
+    },
+    visitTarget: (t: Target<number, number | string>) => {},
+  };
+  y.accept(visitor);
+  w.build();
+  assert.strictEqual(w.value, 'foo4');
 });
