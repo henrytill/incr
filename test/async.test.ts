7a6a9cbc942ccf72fce107f27d208caf22f1dd1a
Henry Till <henrytill@gmail.com>
2023-11-29 00:47:33 -0800

    add AsyncComputable with Cell test

--- test/async.test.ts
+++ test/async.test.ts
@@ -2,6 +2,7 @@ import assert from 'node:assert';
 import test from 'node:test';
 
 import { AsyncCell, AsyncAutoCell, AsyncComputable } from '../src/async.js';
+import { Cell } from '../src/core.js';
 
 test('AsyncCell', async () => {
   const z = AsyncCell.resolve(1);
@@ -53,4 +54,29 @@ test('AsyncAutoCell', async () => {
   await z.value;
 
   assert.strictEqual(await t.value, 11);
+});
+
+test('AsyncComputable with Cell', async () => {
+  const z = new Cell(1);
+  const y = new Cell(2);
+
+  const x = new AsyncComputable([y, z], async (a, b) => {
+    return a.value + b.value;
+  });
+
+  const w = new Cell(3);
+  const v = new AsyncComputable([w, x], async (a, b) => {
+    return a.value + (await b.value);
+  });
+
+  const u = new Cell(4);
+  const t = new AsyncComputable([u, v], async (a, b) => {
+    return a.value + (await b.value);
+  });
+
+  assert.strictEqual(await t.compute().value, 10);
+
+  z.value = 2;
+
+  assert.strictEqual(await t.compute().value, 11);
 });
