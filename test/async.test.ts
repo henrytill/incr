e1374596a758141d27fb7f1396bf1d1581e31eae
Henry Till <henrytill@gmail.com>
2023-11-28 19:54:52 -0800

    add AsyncAutoCell

---
+++ test/async.test.ts
@@ -0,0 +1,21 @@
+import assert from 'node:assert';
+import test from 'node:test';
+
+import { AsyncAutoCell, AsyncComputable } from '../src/async.js';
+
+test('AsyncAutoCell', async () => {
+  const x = new AsyncAutoCell(Promise.resolve(1));
+  const y = new AsyncAutoCell(Promise.resolve(2));
+
+  const z = new AsyncComputable([x, y], async (a, b) => {
+    const [x, y] = await Promise.all([a.value, b.value]);
+    return x + y;
+  }).compute();
+
+  assert.strictEqual(await z.value, 3);
+
+  x.value = Promise.resolve(2);
+  await x.value;
+
+  assert.strictEqual(await z.value, 4);
+});
