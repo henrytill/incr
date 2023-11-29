ac4a4f98e91566b2acdd55734b52720ca60dc7e8
Henry Till <henrytill@gmail.com>
2023-11-28 19:54:52 -0800

    add AsyncAutoCell

--- src/async.ts
+++ src/async.ts
@@ -16,3 +16,25 @@ export class AsyncCell<T> extends Cell<Promise<T>> {
 }
 
 export class AsyncComputable<T> extends Computable<Promise<T>> {}
+
+export class AsyncAutoCell<T> extends Cell<Promise<T>> {
+  constructor(value: Promise<T>, key?: string) {
+    super(value, key);
+  }
+
+  override set value(value: Promise<T>) {
+    this._value = Promise.all([this._value, value]).then(([curr, next]) => {
+      if (curr !== next) {
+        this._value = Promise.resolve(next);
+        this.update();
+      }
+      const roots = this.roots();
+      console.debug('Rebuilding', roots.map((root) => root.key).join(', '));
+      return Promise.all(roots.map((root) => root.compute().value)).then(() => this._value);
+    });
+  }
+
+  override get value(): Promise<T> {
+    return super.value;
+  }
+}
