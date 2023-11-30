d1a42159d9afe8c47c630ff91cd97ea83819be53
Henry Till <henrytill@gmail.com>
2023-11-29 16:12:04 -0800

    convert tests to describe/it syntax

--- test/async.test.ts
+++ test/async.test.ts
@@ -1,82 +1,84 @@
 import assert from 'node:assert';
-import test from 'node:test';
+import { describe, it } from 'node:test';
 
 import { AsyncCell, AsyncAutoCell, AsyncComputable } from '../src/async.js';
 import { Cell } from '../src/core.js';
 
-test('AsyncCell', async () => {
-  const z = AsyncCell.resolve(1);
-  const y = AsyncCell.resolve(2);
+describe('AsyncComputable', () => {
+  it('works with Cell', async () => {
+    const z = new Cell(1);
+    const y = new Cell(2);
 
-  const x = new AsyncComputable([y, z], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    const x = new AsyncComputable([y, z], async (a, b) => {
+      return a.value + b.value;
+    });
+
+    const w = new Cell(3);
+    const v = new AsyncComputable([w, x], async (a, b) => {
+      return a.value + (await b.value);
+    });
+
+    const u = new Cell(4);
+    const t = new AsyncComputable([u, v], async (a, b) => {
+      return a.value + (await b.value);
+    });
+
+    assert.strictEqual(await t.compute().value, 10);
+
+    z.value = 2;
+
+    assert.strictEqual(await t.compute().value, 11);
   });
 
-  const w = AsyncCell.resolve(3);
-  const v = new AsyncComputable([w, x], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+  it('works with AsyncCell', async () => {
+    const z = AsyncCell.resolve(1);
+    const y = AsyncCell.resolve(2);
+
+    const x = new AsyncComputable([y, z], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    const w = AsyncCell.resolve(3);
+    const v = new AsyncComputable([w, x], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    const u = AsyncCell.resolve(4);
+    const t = new AsyncComputable([u, v], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    assert.strictEqual(await t.compute().value, 10);
+
+    z.value = Promise.resolve(2);
+    await z.value;
+
+    assert.strictEqual(await t.compute().value, 11);
   });
 
-  const u = AsyncCell.resolve(4);
-  const t = new AsyncComputable([u, v], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+  it('works with AsyncAutoCell', async () => {
+    const z = AsyncAutoCell.resolve(1);
+    const y = AsyncAutoCell.resolve(2);
+
+    const x = new AsyncComputable([y, z], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    const w = AsyncAutoCell.resolve(3);
+    const v = new AsyncComputable([w, x], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    const u = AsyncAutoCell.resolve(4);
+    const t = new AsyncComputable([u, v], async (a, b) => {
+      return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
+    });
+
+    assert.strictEqual(await t.compute().value, 10);
+
+    z.value = Promise.resolve(2);
+    await z.value;
+
+    assert.strictEqual(await t.value, 11);
   });
-
-  assert.strictEqual(await t.compute().value, 10);
-
-  z.value = Promise.resolve(2);
-  await z.value;
-
-  assert.strictEqual(await t.compute().value, 11);
-});
-
-test('AsyncAutoCell', async () => {
-  const z = AsyncAutoCell.resolve(1);
-  const y = AsyncAutoCell.resolve(2);
-
-  const x = new AsyncComputable([y, z], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
-  });
-
-  const w = AsyncAutoCell.resolve(3);
-  const v = new AsyncComputable([w, x], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
-  });
-
-  const u = AsyncAutoCell.resolve(4);
-  const t = new AsyncComputable([u, v], async (a, b) => {
-    return await Promise.all([a.value, b.value]).then(([x, y]) => x + y);
-  });
-
-  assert.strictEqual(await t.compute().value, 10);
-
-  z.value = Promise.resolve(2);
-  await z.value;
-
-  assert.strictEqual(await t.value, 11);
-});
-
-test('AsyncComputable with Cell', async () => {
-  const z = new Cell(1);
-  const y = new Cell(2);
-
-  const x = new AsyncComputable([y, z], async (a, b) => {
-    return a.value + b.value;
-  });
-
-  const w = new Cell(3);
-  const v = new AsyncComputable([w, x], async (a, b) => {
-    return a.value + (await b.value);
-  });
-
-  const u = new Cell(4);
-  const t = new AsyncComputable([u, v], async (a, b) => {
-    return a.value + (await b.value);
-  });
-
-  assert.strictEqual(await t.compute().value, 10);
-
-  z.value = 2;
-
-  assert.strictEqual(await t.compute().value, 11);
 });
--- test/core.test.ts
+++ test/core.test.ts
@@ -1,101 +1,103 @@
 import assert from 'node:assert';
-import test from 'node:test';
+import { describe, it } from 'node:test';
 
 import { NodeVisitor, Cell, Computable, AutoCell } from '../src/core.js';
 
-test('basic', () => {
-  let count = 0;
+describe('Computable', () => {
+  it('correctly computes', () => {
+    let count = 0;
 
-  const y = new Cell(2);
-  const z = new Cell(3);
-  const x = new Computable([y, z], function (this: any, a, b): number {
-    count += 1;
-    return a.value + b.value;
-  }).compute();
-
-  assert.strictEqual(x.value, 5);
-  assert.strictEqual(x.parents.length, 0);
-  assert.deepStrictEqual(x.children, [y, z]);
-  assert.deepStrictEqual(y.parents, [x]);
-  assert.deepStrictEqual(z.parents, [x]);
-
-  y.value = 4;
-  assert.strictEqual(x.compute().value, 7);
-
-  z.value = 5;
-  assert.strictEqual(x.compute().value, 9);
-
-  assert.strictEqual(count, 3);
-
-  z.value = 5;
-  assert.strictEqual(x.compute().value, 9);
-
-  assert.strictEqual(count, 3);
-});
-
-test('compound', () => {
-  const z = new Cell(3, 'z');
-  const y = new Cell(2, 'y');
-  const x = new Computable(
-    [y, z],
-    function (this: any, a, b): number {
+    const y = new Cell(2);
+    const z = new Cell(3);
+    const x = new Computable([y, z], function (this: any, a, b): number {
+      count += 1;
       return a.value + b.value;
-    },
-    'x',
-  ).compute();
-  const w = new Cell('results', 'w');
-  const v = new Computable([w, x], (a, b) => `${a.value}: ${b.key} is ${b.value}`, 'v').compute();
+    }).compute();
 
-  assert.strictEqual(v.value, 'results: x is 5');
-  assert.strictEqual(v.parents.length, 0);
-  assert.deepStrictEqual(x.parents, [v]);
-  assert.deepStrictEqual(w.parents, [v]);
-  assert.deepStrictEqual(v.children, [w, x]);
+    assert.strictEqual(x.value, 5);
+    assert.strictEqual(x.parents.length, 0);
+    assert.deepStrictEqual(x.children, [y, z]);
+    assert.deepStrictEqual(y.parents, [x]);
+    assert.deepStrictEqual(z.parents, [x]);
 
-  assert.strictEqual(v.key, 'v');
-  assert.strictEqual(w.key, 'w');
-  assert.strictEqual(x.key, 'x');
-  assert.strictEqual(y.key, 'y');
-  assert.strictEqual(z.key, 'z');
+    y.value = 4;
+    assert.strictEqual(x.compute().value, 7);
 
-  const visitor: NodeVisitor<number, number | undefined> = {
-    visitCell: (node: Cell<number>) => {
-      node.value += 1;
-      return node.value;
-    },
-    visitComputable: (_node: Computable<number>) => {
-      return undefined;
-    },
-  };
-  const ret = y.accept(visitor);
-  assert.strictEqual(ret, 3);
-  assert.strictEqual(x.shouldRebuild, true);
-  assert.strictEqual(v.shouldRebuild, true);
-  assert.strictEqual(v.compute().value, 'results: x is 6');
+    z.value = 5;
+    assert.strictEqual(x.compute().value, 9);
 
-  w.value = 'hello';
-  assert.strictEqual(x.shouldRebuild, false);
-  assert.strictEqual(v.shouldRebuild, true);
-  assert.strictEqual(v.compute().value, 'hello: x is 6');
-});
+    assert.strictEqual(count, 3);
 
-test('auto', () => {
-  let count = 0;
+    z.value = 5;
+    assert.strictEqual(x.compute().value, 9);
 
-  const z = new AutoCell(3);
-  const y = new AutoCell(2);
-  const x = new Computable([y, z], function (a, b): number {
-    count += 1;
-    return a.value + b.value;
-  }).compute();
-  const w = new AutoCell(4);
-  const v = new Computable([w, x], (a, b) => a.value + b.value).compute();
+    assert.strictEqual(count, 3);
+  });
 
-  assert.strictEqual(v.value, 9);
-  assert.strictEqual(count, 1);
+  it('correctly computes a larger example', () => {
+    const z = new Cell(3, 'z');
+    const y = new Cell(2, 'y');
+    const x = new Computable(
+      [y, z],
+      function (this: any, a, b): number {
+        return a.value + b.value;
+      },
+      'x',
+    ).compute();
+    const w = new Cell('results', 'w');
+    const v = new Computable([w, x], (a, b) => `${a.value}: ${b.key} is ${b.value}`, 'v').compute();
 
-  w.value = 5;
+    assert.strictEqual(v.value, 'results: x is 5');
+    assert.strictEqual(v.parents.length, 0);
+    assert.deepStrictEqual(x.parents, [v]);
+    assert.deepStrictEqual(w.parents, [v]);
+    assert.deepStrictEqual(v.children, [w, x]);
 
-  assert.strictEqual(v.value, 10);
-  assert.strictEqual(count, 1);
+    assert.strictEqual(v.key, 'v');
+    assert.strictEqual(w.key, 'w');
+    assert.strictEqual(x.key, 'x');
+    assert.strictEqual(y.key, 'y');
+    assert.strictEqual(z.key, 'z');
+
+    const visitor: NodeVisitor<number, number | undefined> = {
+      visitCell: (node: Cell<number>) => {
+        node.value += 1;
+        return node.value;
+      },
+      visitComputable: (_node: Computable<number>) => {
+        return undefined;
+      },
+    };
+    const ret = y.accept(visitor);
+    assert.strictEqual(ret, 3);
+    assert.strictEqual(x.shouldRebuild, true);
+    assert.strictEqual(v.shouldRebuild, true);
+    assert.strictEqual(v.compute().value, 'results: x is 6');
+
+    w.value = 'hello';
+    assert.strictEqual(x.shouldRebuild, false);
+    assert.strictEqual(v.shouldRebuild, true);
+    assert.strictEqual(v.compute().value, 'hello: x is 6');
+  });
+
+  it('works with AutoCell', () => {
+    let count = 0;
+
+    const z = new AutoCell(3);
+    const y = new AutoCell(2);
+    const x = new Computable([y, z], function (a, b): number {
+      count += 1;
+      return a.value + b.value;
+    }).compute();
+    const w = new AutoCell(4);
+    const v = new Computable([w, x], (a, b) => a.value + b.value).compute();
+
+    assert.strictEqual(v.value, 9);
+    assert.strictEqual(count, 1);
+
+    w.value = 5;
+
+    assert.strictEqual(v.value, 10);
+    assert.strictEqual(count, 1);
+  });
 });
