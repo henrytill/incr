16e40e7dd112677c4eee3dc82f7cca466b095e36
Henry Till <henrytill@gmail.com>
2023-12-30 17:10:40 -0800

    .eslintrc.cjs: reformat

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { project: ['./tsconfig.json'] },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
-  globals: {},
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: { project: ['./tsconfig.json'] },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+    },
+    globals: {},
 };
