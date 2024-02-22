d482ff07c2a1635357a909afdb44cd0ccfc83ed1
Henry Till <henrytill@gmail.com>
2024-02-22 11:56:04 -0800

    update .prettierrc.cjs and reformat

--- src/build.ts
+++ src/build.ts
@@ -9,130 +9,130 @@ import { AsyncComputable, AutoCell, Cell } from './core.js';
 export type HashDigest = string;
 
 export type Message = {
-    filename: PathLike;
-    event: FileChangeInfo<string>;
+  filename: PathLike;
+  event: FileChangeInfo<string>;
 };
 
 export type BuildNode = FileCell | Input | AutoInput | Target;
 
 export class FileCell extends Cell<HashDigest> {
-    private constructor(value: HashDigest, key: string) {
-        super(value, key);
-    }
+  private constructor(value: HashDigest, key: string) {
+    super(value, key);
+  }
 
-    static async of(filename: PathLike): Promise<FileCell> {
-        const value = await fs.readFile(filename).then(hash);
-        const ret = new FileCell(value, filename.toString());
-        return ret;
-    }
+  static async of(filename: PathLike): Promise<FileCell> {
+    const value = await fs.readFile(filename).then(hash);
+    const ret = new FileCell(value, filename.toString());
+    return ret;
+  }
 }
 
 export class Input extends Cell<HashDigest> {
-    readonly watcher: Promise<void>;
-    readonly controller = new AbortController();
+  readonly watcher: Promise<void>;
+  readonly controller = new AbortController();
 
-    private constructor(
-        value: HashDigest,
-        key: string,
-        readonly notifications?: Channel<Message>,
-    ) {
-        super(value, key);
-        this.watcher = watch(this, this.controller.signal);
-    }
+  private constructor(
+    value: HashDigest,
+    key: string,
+    readonly notifications?: Channel<Message>,
+  ) {
+    super(value, key);
+    this.watcher = watch(this, this.controller.signal);
+  }
 
-    static async of(filename: PathLike, notifications?: Channel<Message>): Promise<Input> {
-        const value = await fs.readFile(filename).then(hash);
-        const ret = new Input(value, filename.toString(), notifications);
-        return ret;
-    }
+  static async of(filename: PathLike, notifications?: Channel<Message>): Promise<Input> {
+    const value = await fs.readFile(filename).then(hash);
+    const ret = new Input(value, filename.toString(), notifications);
+    return ret;
+  }
 
-    static from(file: FileCell, notifications?: Channel<Message>): Input {
-        const ret = new Input(file.value, file.key, notifications);
-        ret.parents = file.parents;
-        for (const parent of ret.parents) {
-            const i = parent.children.findIndex((child) => child === file);
-            assert.notEqual(i, -1);
-            parent.children[i] = ret;
-        }
-        file.parents = [];
-        return ret;
+  static from(file: FileCell, notifications?: Channel<Message>): Input {
+    const ret = new Input(file.value, file.key, notifications);
+    ret.parents = file.parents;
+    for (const parent of ret.parents) {
+      const i = parent.children.findIndex((child) => child === file);
+      assert.notEqual(i, -1);
+      parent.children[i] = ret;
     }
+    file.parents = [];
+    return ret;
+  }
 
-    async close(): Promise<void> {
-        this.controller.abort();
-        await this.watcher;
-    }
+  async close(): Promise<void> {
+    this.controller.abort();
+    await this.watcher;
+  }
 }
 
 export class AutoInput extends AutoCell<HashDigest> {
-    readonly watcher: Promise<void>;
-    readonly controller = new AbortController();
+  readonly watcher: Promise<void>;
+  readonly controller = new AbortController();
 
-    private constructor(
-        value: HashDigest,
-        key: string,
-        readonly notifications?: Channel<Message>,
-    ) {
-        super(value, key);
-        this.watcher = watch(this, this.controller.signal);
-    }
+  private constructor(
+    value: HashDigest,
+    key: string,
+    readonly notifications?: Channel<Message>,
+  ) {
+    super(value, key);
+    this.watcher = watch(this, this.controller.signal);
+  }
 
-    static async of(filename: PathLike, notifications?: Channel<Message>): Promise<AutoInput> {
-        const value = await fs.readFile(filename).then(hash);
-        const ret = new AutoInput(value, filename.toString(), notifications);
-        return ret;
-    }
+  static async of(filename: PathLike, notifications?: Channel<Message>): Promise<AutoInput> {
+    const value = await fs.readFile(filename).then(hash);
+    const ret = new AutoInput(value, filename.toString(), notifications);
+    return ret;
+  }
 
-    static from(file: FileCell, notifications?: Channel<Message>): AutoInput {
-        const ret = new AutoInput(file.value, file.key, notifications);
-        ret.parents = file.parents;
-        for (const parent of ret.parents) {
-            const i = parent.children.findIndex((child) => child === file);
-            assert.notEqual(i, -1);
-            parent.children[i] = ret;
-        }
-        file.parents = [];
-        return ret;
+  static from(file: FileCell, notifications?: Channel<Message>): AutoInput {
+    const ret = new AutoInput(file.value, file.key, notifications);
+    ret.parents = file.parents;
+    for (const parent of ret.parents) {
+      const i = parent.children.findIndex((child) => child === file);
+      assert.notEqual(i, -1);
+      parent.children[i] = ret;
     }
+    file.parents = [];
+    return ret;
+  }
 
-    async close(): Promise<void> {
-        this.controller.abort();
-        await this.watcher;
-    }
+  async close(): Promise<void> {
+    this.controller.abort();
+    await this.watcher;
+  }
 }
 
 export class Target extends AsyncComputable<HashDigest> {}
 
 export function hash(input: BinaryLike): HashDigest {
-    const hash = crypto.createHash('sha256');
-    hash.update(input);
-    return hash.digest('hex');
+  const hash = crypto.createHash('sha256');
+  hash.update(input);
+  return hash.digest('hex');
 }
 
 /** Implements leading-edge debounce on an asynchronous event stream. */
 async function* debounce<T>(events: AsyncIterable<T>, delay: number): AsyncGenerator<T> {
-    let begin = Date.now() - delay; // allow first event to pass through
-    for await (const event of events) {
-        const now = Date.now();
-        if (now - begin >= delay) {
-            begin = now;
-            yield event;
-        }
+  let begin = Date.now() - delay; // allow first event to pass through
+  for await (const event of events) {
+    const now = Date.now();
+    if (now - begin >= delay) {
+      begin = now;
+      yield event;
     }
+  }
 }
 
 async function watch(input: Input, signal: AbortSignal): Promise<void> {
-    const filename = input.key;
-    try {
-        const watcher = fs.watch(filename, { signal });
-        for await (const event of debounce(watcher, 1000)) {
-            input.value = await fs.readFile(filename).then(hash);
-            input.notifications?.send({ filename, event });
-        }
-    } catch (err: any) {
-        if (err.name === 'AbortError') {
-            return;
-        }
-        throw err;
+  const filename = input.key;
+  try {
+    const watcher = fs.watch(filename, { signal });
+    for await (const event of debounce(watcher, 1000)) {
+      input.value = await fs.readFile(filename).then(hash);
+      input.notifications?.send({ filename, event });
     }
+  } catch (err: any) {
+    if (err.name === 'AbortError') {
+      return;
+    }
+    throw err;
+  }
 }
--- test/core.test.ts
+++ test/core.test.ts
@@ -4,131 +4,127 @@ import { describe, it } from 'node:test';
 import { NodeVisitor, Cell, Computable, AutoCell, AsyncComputable } from '../src/core.js';
 
 describe('Computable', () => {
-    it('correctly computes', () => {
-        let count = 0;
+  it('correctly computes', () => {
+    let count = 0;
 
-        const y = new Cell(2);
-        const z = new Cell(3);
-        const x = new Computable([y, z], function (this: any, a, b): number {
-            count += 1;
-            return a.value + b.value;
-        }).compute();
+    const y = new Cell(2);
+    const z = new Cell(3);
+    const x = new Computable([y, z], function (this: any, a, b): number {
+      count += 1;
+      return a.value + b.value;
+    }).compute();
 
-        assert.equal(x.value, 5);
-        assert.equal(x.parents.length, 0);
-        assert.deepEqual(x.children, [y, z]);
-        assert.deepEqual(y.parents, [x]);
-        assert.deepEqual(z.parents, [x]);
+    assert.equal(x.value, 5);
+    assert.equal(x.parents.length, 0);
+    assert.deepEqual(x.children, [y, z]);
+    assert.deepEqual(y.parents, [x]);
+    assert.deepEqual(z.parents, [x]);
 
-        y.value = 4;
-        assert.equal(x.compute().value, 7);
+    y.value = 4;
+    assert.equal(x.compute().value, 7);
 
-        z.value = 5;
-        assert.equal(x.compute().value, 9);
+    z.value = 5;
+    assert.equal(x.compute().value, 9);
 
-        assert.equal(count, 3);
+    assert.equal(count, 3);
 
-        z.value = 5;
-        assert.equal(x.compute().value, 9);
+    z.value = 5;
+    assert.equal(x.compute().value, 9);
 
-        assert.equal(count, 3);
-    });
+    assert.equal(count, 3);
+  });
 
-    it('correctly computes a larger example', () => {
-        const z = new Cell(3, 'z');
-        const y = new Cell(2, 'y');
-        const x = new Computable(
-            [y, z],
-            function (this: any, a, b): number {
-                return a.value + b.value;
-            },
-            'x',
-        ).compute();
-        const w = new Cell('results', 'w');
-        const v = new Computable(
-            [w, x],
-            (a, b) => `${a.value}: ${b.key} is ${b.value}`,
-            'v',
-        ).compute();
+  it('correctly computes a larger example', () => {
+    const z = new Cell(3, 'z');
+    const y = new Cell(2, 'y');
+    const x = new Computable(
+      [y, z],
+      function (this: any, a, b): number {
+        return a.value + b.value;
+      },
+      'x',
+    ).compute();
+    const w = new Cell('results', 'w');
+    const v = new Computable([w, x], (a, b) => `${a.value}: ${b.key} is ${b.value}`, 'v').compute();
 
-        assert.equal(v.value, 'results: x is 5');
-        assert.equal(v.parents.length, 0);
-        assert.deepEqual(x.parents, [v]);
-        assert.deepEqual(w.parents, [v]);
-        assert.deepEqual(v.children, [w, x]);
+    assert.equal(v.value, 'results: x is 5');
+    assert.equal(v.parents.length, 0);
+    assert.deepEqual(x.parents, [v]);
+    assert.deepEqual(w.parents, [v]);
+    assert.deepEqual(v.children, [w, x]);
 
-        assert.equal(v.key, 'v');
-        assert.equal(w.key, 'w');
-        assert.equal(x.key, 'x');
-        assert.equal(y.key, 'y');
-        assert.equal(z.key, 'z');
+    assert.equal(v.key, 'v');
+    assert.equal(w.key, 'w');
+    assert.equal(x.key, 'x');
+    assert.equal(y.key, 'y');
+    assert.equal(z.key, 'z');
 
-        const visitor: NodeVisitor<number, number | undefined> = {
-            visitCell: (node: Cell<number>) => {
-                node.value += 1;
-                return node.value;
-            },
-            visitComputable: (_node: Computable<number>) => {
-                return undefined;
-            },
-        };
-        const ret = y.accept(visitor);
-        assert.equal(ret, 3);
-        assert.equal(x.shouldRebuild, true);
-        assert.equal(v.shouldRebuild, true);
-        assert.equal(v.compute().value, 'results: x is 6');
+    const visitor: NodeVisitor<number, number | undefined> = {
+      visitCell: (node: Cell<number>) => {
+        node.value += 1;
+        return node.value;
+      },
+      visitComputable: (_node: Computable<number>) => {
+        return undefined;
+      },
+    };
+    const ret = y.accept(visitor);
+    assert.equal(ret, 3);
+    assert.equal(x.shouldRebuild, true);
+    assert.equal(v.shouldRebuild, true);
+    assert.equal(v.compute().value, 'results: x is 6');
 
-        w.value = 'hello';
-        assert.equal(x.shouldRebuild, false);
-        assert.equal(v.shouldRebuild, true);
-        assert.equal(v.compute().value, 'hello: x is 6');
-    });
+    w.value = 'hello';
+    assert.equal(x.shouldRebuild, false);
+    assert.equal(v.shouldRebuild, true);
+    assert.equal(v.compute().value, 'hello: x is 6');
+  });
 
-    it('works with AutoCell', () => {
-        let count = 0;
+  it('works with AutoCell', () => {
+    let count = 0;
 
-        const z = new AutoCell(3);
-        const y = new AutoCell(2);
-        const x = new Computable([y, z], function (a, b): number {
-            count += 1;
-            return a.value + b.value;
-        }).compute();
-        const w = new AutoCell(4);
-        const v = new Computable([w, x], (a, b) => a.value + b.value).compute();
+    const z = new AutoCell(3);
+    const y = new AutoCell(2);
+    const x = new Computable([y, z], function (a, b): number {
+      count += 1;
+      return a.value + b.value;
+    }).compute();
+    const w = new AutoCell(4);
+    const v = new Computable([w, x], (a, b) => a.value + b.value).compute();
 
-        assert.equal(v.value, 9);
-        assert.equal(count, 1);
+    assert.equal(v.value, 9);
+    assert.equal(count, 1);
 
-        w.value = 5;
+    w.value = 5;
 
-        assert.equal(v.value, 10);
-        assert.equal(count, 1);
-    });
+    assert.equal(v.value, 10);
+    assert.equal(count, 1);
+  });
 });
 
 describe('AsyncComputable', () => {
-    it('works with Cell', async () => {
-        const z = new Cell(1);
-        const y = new Cell(2);
+  it('works with Cell', async () => {
+    const z = new Cell(1);
+    const y = new Cell(2);
 
-        const x = new AsyncComputable([y, z], async (a, b) => {
-            return a.value + b.value;
-        });
-
-        const w = new Cell(3);
-        const v = new AsyncComputable([w, x], async (a, b) => {
-            return a.value + (await b.value);
-        });
-
-        const u = new Cell(4);
-        const t = new AsyncComputable([u, v], async (a, b) => {
-            return a.value + (await b.value);
-        });
-
-        assert.equal(await t.compute().value, 10);
-
-        z.value = 2;
-
-        assert.equal(await t.compute().value, 11);
+    const x = new AsyncComputable([y, z], async (a, b) => {
+      return a.value + b.value;
     });
+
+    const w = new Cell(3);
+    const v = new AsyncComputable([w, x], async (a, b) => {
+      return a.value + (await b.value);
+    });
+
+    const u = new Cell(4);
+    const t = new AsyncComputable([u, v], async (a, b) => {
+      return a.value + (await b.value);
+    });
+
+    assert.equal(await t.compute().value, 10);
+
+    z.value = 2;
+
+    assert.equal(await t.compute().value, 11);
+  });
 });
