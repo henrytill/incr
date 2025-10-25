7a4ab594d0c0aa43b08cc7420add5199185124f1
Henry Till <henrytill@gmail.com>
2025-10-24 18:34:44 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        src = builtins.path {
+          path = ./.;
+          name = "incr-src";
+        };
+        npmRoot = src;
+        incr = pkgs.buildNpmPackage {
+          pname = "incr";
+          version = "0.1.0";
+          inherit src;
+
+          npmDeps = pkgs.importNpmLock { inherit npmRoot; };
+
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+        };
+      in
+      {
+        packages.default = incr;
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            importNpmLock.hooks.linkNodeModulesHook
+            nodejs
+          ];
+          npmDeps = pkgs.importNpmLock.buildNodeModules {
+            inherit npmRoot;
+            inherit (pkgs) nodejs;
+          };
+        };
+      }
+    );
+}
